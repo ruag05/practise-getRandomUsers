40d040054b83ee9686d8b564c9301cd45739470b
ruag05 <ruchiragarwal05@gmail.com>
2021-10-25 02:14:47 -0700

    Practiced fetching data using Axios and displaying as Plain JSON

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
+const fetchUserData = () => {
+    return axios.get('https://randomuser.me/api')
+    .then(res => {
+      //console.log(res);
+      return res;
+    })
+}
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [userInfos, setUserInfos] = useState('');
+  const [randomUserDataJSON, setRandomUserDataJSON] = useState();
 
-  function increaseCounter(){
-        setCounter(counter + 1)
-  }
+  useEffect( () => {
+      fetchUserData().then(res => {
+        setRandomUserDataJSON(JSON.stringify(res, null, 3));  
+      })      
+    }, []
+  )
 
   return (
     <div>
       <h2>
         Code SandBox
       </h2>
-      <div>{counter}</div>
-      <button onClick= {increaseCounter}>Increase Counter</button>
+      <pre>
+      {
+        randomUserDataJSON
+      }
+      </pre>
+
     </div>
   );
 }
