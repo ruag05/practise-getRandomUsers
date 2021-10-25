516aa2a7c69de558eb0ed0d5086441a36475f4b9
ruag05 <ruchiragarwal05@gmail.com>
2021-10-25 00:54:42 -0700

    Added simple counter functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function increaseCounter(){
+        setCounter(counter + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>
+        Code SandBox
+      </h2>
+      <div>{counter}</div>
+      <button onClick= {increaseCounter}>Increase Counter</button>
     </div>
   );
 }
